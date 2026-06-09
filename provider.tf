f07086c83236adc945e7cab1a37c350187c63d20
Ugrasen Gangwar <ugrasen.gangwar0210@gmail.com>
2026-06-09 21:16:01 +0530

    Add files via upload

--- provider.tf
+++ provider.tf
@@ -5,12 +5,6 @@ terraform {
       version = "4.74.0"
     }
   }
-#  backend "azurerm" {
-#     resource_group_name = "kp1"
-#     storage_account_name = "krishn022026"
-#     container_name = "container1"
-#     key = 
-# }
 }
 
 provider "azurerm" {
